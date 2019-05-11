6947e3f1ce3a35649754da95ea273035f961603b
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-10 20:54:58 -0500

    Some functionality added to Feelings component

--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import NumericInput from 'react-numeric-input';
+import {connect} from 'react-redux';
 
 class Feelings extends Component {
+
+    addFeelings = (number) => {
+        console.log(number);
+        this.props.dispatch({type: 'ADD_FEELINGS', payload: number})
+    }
+
     render() {
         return (
             <div>
             <h2>How are you feeling today?</h2>
-            <NumericInput min={0} max={5} />
+            <NumericInput min={0} max={5} placeholder="Number 1 through 5" />
             </div>
         )
     }
 }
 
-export default Feelings;
+export default connect()(Feelings);
