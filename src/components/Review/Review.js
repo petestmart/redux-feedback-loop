c4ec7b6b5a34a153c1632182024ebd0a9fafd8f5
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-12 10:34:54 -0500

    review component started

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
 
 class Review extends Component {
     render() {
         return (
             <div>
             <p>Review Component Placeholder</p>
+            <p>Feeling: {this.props.reduxState.finalSubmitReducer.feelings}</p>
+            <p>Understanding:</p>
+            <p>Support:</p>
+            <p>Comments:</p>
             </div>
         )
     }
 }
 
-export default Review;
+const mapStateToProps = (reduxState) => {
+    // this.props.reduxState
+    return {
+        reduxState
+    }
+}
+
+export default connect(mapStateToProps)(Review);
