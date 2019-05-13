7f1644490b648c8f3f06ec420c1083cf8691e7e4
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-12 20:36:16 -0500

    Success Page button brings user home to home page and clears inputs

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Success extends Component {
+
+    newFeedback = () => {
+        this.props.dispatch({ type: 'ADD_FEELINGS', payload: [] });
+        this.props.dispatch({ type: 'ADD_UNDERSTANDING', payload: [] });
+        this.props.dispatch({ type: 'ADD_SUPPORT', payload: [] });
+        this.props.dispatch({ type: 'ADD_COMMENTS', payload: [] });
+        this.props.history.push('/');
+    }
 
     render() {
         return (
             <div>
                 <h2>Thank You!</h2>
-                <button>Leave New Feedback</button>
+                <button onClick={this.newFeedback}>Leave New Feedback</button>
             </div>
         )
     }
 
 }
 
-export default Success;
+export default connect()(Success);
